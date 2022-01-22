d768c7c6027cd61515cd603699b227e39b2ecaa8
Carlos Ramirez <carlos@extrasistemas.com>
2022-01-22 01:05:17 -0600

    added the print search value

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -2,17 +2,21 @@ import React from 'react';
 import './TodoSearch.css';
 
 function TodoSearch() {
-  const onSearchValueChange = (event) => {
-    console.log(event.target.value);
-  };
-  
-  return (
+
+  const  [searchValue, setSearchValue] =  React.useState('')
+  const onSearchValueChange = (e)=> {
+    console.log(e.target.value)
+    setSearchValue(e.target.value)
+  }
+  return [
     <input
       className="TodoSearch"
-      placeholder="Cebolla"
+      placeholder="Search..."
+      value={searchValue}
       onChange={onSearchValueChange}
-    />
-  );
+    />,
+    <p>{searchValue}</p>
+  ]
 }
 
 export { TodoSearch };
